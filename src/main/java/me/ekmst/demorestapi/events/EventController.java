336c4bd783dc2213b73e4d70e88e7e945cfae901
kyeongmin <ekmst1027@gmail.com>
2019-12-01 16:46:44 +0900

    Implement GET /api

--- src/main/java/me/ekmst/demorestapi/events/EventController.java
+++ src/main/java/me/ekmst/demorestapi/events/EventController.java
@@ -1,5 +1,6 @@
 package me.ekmst.demorestapi.events;
 
+import me.ekmst.demorestapi.common.ErrorsResource;
 import org.modelmapper.ModelMapper;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.MediaTypes;
@@ -38,12 +39,12 @@ public class EventController {
     @PostMapping
     public ResponseEntity createEvent(@Valid @RequestBody EventDto eventDto, Errors errors) {
         if(errors.hasErrors()){
-            return ResponseEntity.badRequest().body(errors);
+            return badRequest(errors);
         }
 
         eventValidator.validate(eventDto, errors);
         if(errors.hasErrors()){
-            return ResponseEntity.badRequest().body(errors);
+            return badRequest(errors);
         }
 
         Event event = modelMapper.map(eventDto, Event.class);
@@ -54,10 +55,13 @@ public class EventController {
         URI createdUri = selfLinkBuilder.toUri();
         EventResource eventResource = new EventResource(event);
         eventResource.add(linkTo(EventController.class).withRel("query-events"));
-        eventResource.add(selfLinkBuilder.withSelfRel());
         eventResource.add(selfLinkBuilder.withRel("update-event"));
         eventResource.add(new Link("/docs/index.html#resources-events-create").withRel("profile"));
         return ResponseEntity.created(createdUri).body(eventResource);
+    }
+
+    private ResponseEntity badRequest(Errors errors) {
+        return ResponseEntity.badRequest().body(new ErrorsResource(errors));
     }
 
 }
--- src/main/java/me/ekmst/demorestapi/events/EventResource.java
+++ src/main/java/me/ekmst/demorestapi/events/EventResource.java
@@ -1,18 +1,14 @@
 package me.ekmst.demorestapi.events;
 
-import com.fasterxml.jackson.annotation.JsonUnwrapped;
-import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
 
-public class EventResource extends RepresentationModel {
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 
-    @JsonUnwrapped
-    private Event event;
+public class EventResource extends EntityModel<Event> {
 
-    public EventResource(Event event) {
-        this.event = event;
-    }
-
-    public Event getEvent() {
-        return event;
+    public EventResource(Event event, Link... links) {
+        super(event, links);
+        add(linkTo(EventController.class).slash(event.getId()).withSelfRel());
     }
 }
