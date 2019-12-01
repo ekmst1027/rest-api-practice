70c3b2457d1c1fdc2bcc1a6b541bb7b379b9d590
kyeongmin <ekmst1027@gmail.com>
2019-12-01 12:56:42 +0900

    Add test description annotation

--- src/test/java/me/ekmst/demorestapi/events/EventControllerTests.java
+++ src/test/java/me/ekmst/demorestapi/events/EventControllerTests.java
@@ -1,6 +1,7 @@
 package me.ekmst.demorestapi.events;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import me.ekmst.demorestapi.common.TestDescription;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -31,6 +32,7 @@ public class EventControllerTests {
     ObjectMapper objectMapper;
 
     @Test
+    @TestDescription("정상적으로 이벤트를 생성하는 테스트")
     public void createEvent() throws Exception {
         EventDto event = EventDto.builder()
                 .name("Spring")
@@ -60,6 +62,7 @@ public class EventControllerTests {
     }
 
     @Test
+    @TestDescription("입력 받을 수 없는 값을 사용하는 경우 에러가 발생하는 테스트")
     public void createEvent_Bad_Request() throws Exception {
         Event event = Event.builder()
                 .id(100)
@@ -87,6 +90,7 @@ public class EventControllerTests {
     }
 
     @Test
+    @TestDescription("입력 값이 비어있는 경우에 에러가 발생하는 테스트")
     public void createEvent_Bad_Request_Empty_Input() throws Exception {
         EventDto eventDto = EventDto.builder().build();
 
@@ -98,6 +102,7 @@ public class EventControllerTests {
     }
 
     @Test
+    @TestDescription("입력 값이 잘못된 경우에 에러가 발생하는 테스트")
     public void createEvent_Bad_Request_Wrong_Input() throws Exception {
         EventDto eventDto = EventDto.builder()
                 .name("Spring")
